68e3f027a5deec44aff197dfa5bbff3519be3b28
joaorema <joaopcrema@gmail.com>
2025-03-03 16:22:30 +0000

    twosum done

--- TwoSum/twosum.c
+++ TwoSum/twosum.c
@@ -3,15 +3,16 @@
 
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) 
 {
-    int i = 0;  
-    int j = i + 1;
+   int i = 0;
+   int j = i + 1;
 
-    int *result = (int *)malloc(sizeof(int) * 2);
-    *returnSize = 2;
+   int* result = (int*)malloc(sizeof(int) * 2); 
+   *returnSize = 2;
 
-    while(i++ < numsSize - 1)
-    {
-        while(j++ < numsSize - 1)
+    while(i < numsSize)
+    {   
+        j = i + 1;
+        while(j < numsSize)
         {
             if(nums[i] + nums[j] == target)
             {
@@ -19,12 +20,13 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize)
                 result[1] = j;
                 return result;
             }
+            j++;
         }
+        i++;
     }
-    result[0] = 0;
-    result[1] = 1;
-    return result;
-
+result[0] = 0;
+result[1] = 1;
+return result;
 }
 
 /*int main()
@@ -37,20 +39,20 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize)
 
     int *result = twoSum(array, numsSize, target, &returnSize);
     printf("%i , %i\n", result[0], result[1]);
-    
+
 }*/
 
-/*int main()
+int main()
 {
-    int target = 6;
-    int numsSize = 3;
-    int array[] = {3,2,4};
+    int target = 14;
+    int numsSize = 7;
+    int array[] = {3,2,3,9,3,5,7};
     int returnSize;
 
     int *result = twoSum(array, numsSize, target, &returnSize);
     printf("%i , %i\n", result[0], result[1]);
 
-}*/
+}
 
 /*int main()
 {
